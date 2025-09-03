eaef86ec7d85c70df1adf71419708c62dc2ebb66
Guen Prawiroatmodjo <hi@guen.pw>
2025-09-02 17:19:23 -0700

    remove unused icon

--- src/App.js
+++ src/App.js
@@ -15,12 +15,6 @@ const Heart = ({ className, fill = "currentColor", stroke = "currentColor" }) =>
   </svg>
 );
 
-const Dagger = ({ className }) => (
-  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 2l-1 6h2l-1-6zm0 8v12m-3-10l6 0m-6 2l6 0" />
-  </svg>
-);
-
 const Shield = ({ className }) => (
   <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
